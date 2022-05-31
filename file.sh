f4db31fcfe23c14aff3143f8e245d3bd73ec0448
Bogdanna11 <nicolerus33@gmail.com>
2022-05-31 21:17:49 +0100

    adding the file.h

---
+++ file.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+cd app
+cd app
+cd app
+
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
+sudo apt-get install nodejs
+sudo npm install pm2 -g
+
+npm install -d
+npm start
